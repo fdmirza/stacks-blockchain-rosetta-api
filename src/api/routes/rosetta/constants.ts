9626f8ffa6728427bc2aa20af17a445494506e2e
Asim Mehmood <asim@tintash.com>
2020-08-18 14:25:26 +0500

    chore: add rosetta errors list

--- src/api/routes/rosetta/constants.ts
+++ src/api/routes/rosetta/constants.ts
@@ -3,16 +3,3 @@ export class RosettaConstants {
   static readonly network = 'testnet';
   static readonly rosettaVersion = '1.4.2';
 }
-
-export class RosettaErrorCodes {
-  static readonly invalidAccount = 601;
-  static readonly insufficientFunds = 602;
-  static readonly accountEmpty = 603;
-  static readonly invalidBlockIndex = 604;
-  static readonly getBlockFailed = 605;
-  static readonly invalidBlockHash = 606;
-  static readonly getTransactionFailed = 607;
-  static readonly invalidTransactionHash = 608;
-  static readonly invalidParams = 609;
-  static readonly invalidContractAddress = 610;
-}
