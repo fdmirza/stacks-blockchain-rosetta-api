726ba028c5b2e967a4afe1d310245b778ee50e47
Nouman Ahmad <noumanahmad001@gmail.com>
2020-08-20 03:09:38 +0500

    chore: safety checks added for account balance
    errors added for account api

--- src/api/routes/rosetta/account.ts
+++ src/api/routes/rosetta/account.ts
@@ -11,8 +11,16 @@ export function createAccountRouter(db: DataStore): RouterWithAsync {
   router.postAsync('/balance', async (req, res) => {
     const networkIdentifier = req.body.network_identifier;
 
-    if (networkIdentifier == null) {
+    if (!networkIdentifier) {
       res.status(400).json(RosettaErrors.emptyNetworkIdentifier);
+    }
+
+    if (!networkIdentifier.blockchain) {
+      res.status(400).json(RosettaErrors.emptyBlockchain);
+    }
+
+    if (!networkIdentifier.network) {
+      res.status(400).json(RosettaErrors.emptyBlockchain);
     }
 
     if (networkIdentifier.blockchain != RosettaConstants.blockchain) {
@@ -25,32 +33,29 @@ export function createAccountRouter(db: DataStore): RouterWithAsync {
 
     const accountIdentifier = req.body.account_identifier;
 
-    if (accountIdentifier == null) {
+    if (!accountIdentifier) {
       res.status(400).json(RosettaErrors.emptyAccountIdentifier);
     }
 
     const stxAddress = accountIdentifier.address;
 
     if (!isValidPrincipal(accountIdentifier.address)) {
-      return res.status(400).json({ error: `invalid STX address "${stxAddress}"` });
+      return res.status(400).json(RosettaErrors.invalidAccount);
     }
 
     const blockIdentifier = req.body.block_identifier;
     let balance: bigint = BigInt(0);
-    if (blockIdentifier != null) {
-      if (blockIdentifier.index && blockIdentifier.hash) {
-        const result = await db.getStxBalanceAtBlock(
-          stxAddress,
-          //  '\\x' + blockIdentifier.hash,
-          blockIdentifier.index
-        );
-        balance = result.balance;
-      } else {
-        res.status(400).json(RosettaErrors.invalidBlockIdentifier);
-      }
-    } else {
+
+    if (!blockIdentifier) {
       const result = await db.getStxBalance(stxAddress);
       balance = result.balance;
+    }
+
+    if (blockIdentifier.index && blockIdentifier.hash) {
+      const result = await db.getStxBalanceAtBlock(stxAddress, blockIdentifier.index);
+      balance = result.balance;
+    } else {
+      res.status(400).json(RosettaErrors.invalidBlockIdentifier);
     }
 
     // Get balance info for STX token
